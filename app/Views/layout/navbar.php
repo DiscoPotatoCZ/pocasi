2ddb25d3637fd079361e3a87052149a6173ea758
DiscoPotatoCZ <jirka.dvorak2058@seznam.cz>
2024-12-05 08:39:40 +0100

    Update navbar.php

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -6,14 +6,14 @@
   ?>
   <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Počasí</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
-          <?= anchor("pokus", "Domů", $navbar); ?> 
+          <?= anchor("/", "Domů", $navbar); ?> 
               <span class="visually-hidden">(current)</span>
             </a>
           </li>
